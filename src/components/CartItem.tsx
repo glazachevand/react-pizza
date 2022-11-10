4f268fc3bbb1037fc993ea631b0484d8274e8e94
Nadezhda Glazacheva <glaznad75@gmail.com>
2022-11-10 18:08:07 +0700

    v.2 - adaptive cart

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -14,15 +14,7 @@ type CartItemProps = {
   imageUrl: string;
 };
 
-export const CartItem: React.FC<CartItemProps> = ({
-  id,
-  title,
-  price,
-  type,
-  size,
-  count,
-  imageUrl,
-}) => {
+export const CartItem: React.FC<CartItemProps> = ({ id, title, price, type, size, count, imageUrl }) => {
   const dispatch = useDispatch();
 
   const onClickPlus = () => {
@@ -51,16 +43,8 @@ export const CartItem: React.FC<CartItemProps> = ({
         </p>
       </Link>
       <div className="cart__item-count">
-        <button
-          disabled={count === 1}
-          className="button button--outline button--circle cart__item-count-minus"
-          onClick={onClickMinus}>
-          <svg
-            width="10"
-            height="10"
-            viewBox="0 0 10 10"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
+        <button disabled={count === 1} className="button button--outline button--circle cart__item-count-minus" onClick={onClickMinus}>
+          <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
               fill="#EB5A1E"
@@ -72,15 +56,8 @@ export const CartItem: React.FC<CartItemProps> = ({
           </svg>
         </button>
         <b>{count}</b>
-        <button
-          className="button button--outline button--circle cart__item-count-plus"
-          onClick={onClickPlus}>
-          <svg
-            width="10"
-            height="10"
-            viewBox="0 0 10 10"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
+        <button className="button button--outline button--circle cart__item-count-plus" onClick={onClickPlus}>
+          <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
               fill="#EB5A1E"
@@ -97,12 +74,7 @@ export const CartItem: React.FC<CartItemProps> = ({
       </div>
       <div className="cart__item-remove">
         <div className="button button--outline button--circle" onClick={onClickRemove}>
-          <svg
-            width="10"
-            height="10"
-            viewBox="0 0 10 10"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
+          <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
               fill="#EB5A1E"
